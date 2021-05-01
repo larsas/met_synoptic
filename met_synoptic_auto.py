f194ae26539d95ed2e9131cb6476b81ac0e6d838
Lars Andreas  Selberg <larsas@bvglserver-b1.int.met.no>
2021-05-01 19:15:13 +0000

    open mslp file

--- met_synoptic_auto.py
+++ met_synoptic_auto.py
@@ -5,4 +5,19 @@ author: larsas@met.no
 '''
 #Modules
 import numpy as np
+from netCDF4 import Dataset
 
+path = '/lustre/storeB/project/fou/om/STP40/met_synoptic/era5_atm_CDS_201901_msl_area.nc'
+
+openparameter = Dataset(path, mode = 'r')
+
+parameter = openparameter.variables['msl'][:]
+lons = openparameter.variables['longitude'][:]
+lats = openparameter.variables['latitude'][:]
+
+print(np.shape(parameter))
+print(np.shape(lons),np.shape(lats))
+
+
+mslpday0 = parameter[0,:,:]/100
+mslpday1 = parameter[1,:,:]/100
